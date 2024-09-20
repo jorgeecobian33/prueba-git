fba3d63b38ef70b19fc2c781c30e9a3b023e4756
Jorge Cobián <j.cobian@opendeusto.es>
2024-09-20 08:37:31 +0200

    nadfadnfnk

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	
 	public static void main(String[] args) {
 		System.out.println("adios mundo!!!!!!");
-		System.out.println("Esto es otro mensaje");
+		System.out.println("Estfadjljaodjfklaldfjlkd");
 	}
 
 }
